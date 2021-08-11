08006a122cd405cdf6e4b55ca99a00cf0bc88964
sophie <justanothersophie@gmail.com>
2021-08-11 02:03:21 -0400

    Can now load index with Nokogiri

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,13 @@ require_relative '../config/environment.rb'
 
 class Scraper
 
+    def self.scrape_index
+        url = "https://boardgamegeek.com/browse/boardgame"
+        uri = URI.open(url)
+        doc_index = Nokogiri::HTML(uri)
+        puts doc_index.css("tr")
+        # binding.pry
+    end
 end
+
+# Scraper.scrape_index
